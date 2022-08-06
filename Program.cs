d164feea44ed0fb2fe32d32c12d4f7aae7d03e8d
Yung.Energy <morskojroma@gmail.com>
2022-08-06 13:23:48 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = m;
+Console.WriteLine($"M = {m}; N = {n} -> " + SumOfNaturalNumbers(n, m));
+
+int SumOfNaturalNumbers(int m, int n)
+{
+    int max = m,
+        min = n;
+    if (min == max)
+        return min;
+    else
+    {
+        SumOfNaturalNumbers(max - 1, min);
+        return sum += max;
+    }
+}
